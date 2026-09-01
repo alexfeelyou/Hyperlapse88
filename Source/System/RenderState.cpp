7d5898b9ba802cb6a5284222b354eed328266eef
Sani <shiny.richotten@gmail.com>
2026-09-01 14:44:57 +0900

    fix: texture alpha bug caused by engine's Editor mode interacts with ImGui

--- Source/System/RenderState.cpp
+++ Source/System/RenderState.cpp
@@ -152,9 +152,12 @@ RenderState::RenderState(ID3D11Device* device)
 		desc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
 		desc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
 		desc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+
+		// Preserve the alpha of the skin behind the decal
 		desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
-		desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
+		desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_INV_SRC_ALPHA;
 		desc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+
 		desc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
 		HRESULT hr = device->CreateBlendState(&desc,
 			blendStates[static_cast<int>(BlendState::Transparency)].GetAddressOf());
